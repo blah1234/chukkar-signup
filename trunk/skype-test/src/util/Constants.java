0167493461ca3c36bea4dc2ffee92a56a0426631
hwang.shawn@gmail.com <hwang.shawn@gmail.com@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2010-11-18 00:58:10 +0000

    add constructs to aid in calculating time elapsed between change of node's presence and delivery of notification to buddies

--- trunk/skype-test/src/util/Constants.java
+++ trunk/skype-test/src/util/Constants.java
@@ -4,4 +4,5 @@ public interface Constants
 {
 	static final String NODE_GUID = "NODE_GUID";
 	static final String NODE_STATE = "NODE_STATE";
+	static final String NODE_STATE_CHANGE_TIME = "NODE_STATE_CHANGE_TIME";
 }
