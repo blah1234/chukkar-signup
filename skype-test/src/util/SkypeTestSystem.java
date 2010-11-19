141863896b782d5dd8a6749c9802b771808ecd9b
hwang.shawn@gmail.com <hwang.shawn@gmail.com@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2010-11-19 06:47:27 +0000

    move Latency and related methods to Node class

--- skype-test/src/util/SkypeTestSystem.java
+++ skype-test/src/util/SkypeTestSystem.java
@@ -1,15 +1,11 @@
 package util;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.apache.log4j.Logger;
 
 public class SkypeTestSystem
 {
 	//////////////////////////// MEMBER VARIABLES //////////////////////////////
-	static private final Logger _log = Logger.getLogger(SkypeTestSystem.class);
-	static private final Latency _latencyMetric = new Latency();
 	static private final AtomicLong _timeCount = new AtomicLong(-1);
 	static private volatile boolean _shutdown = false;
 	static private final long _nanosPerTimeIncrement;
@@ -49,45 +45,8 @@ public class SkypeTestSystem
 		_shutdown = true;
 	}
 	
-	public static void addNotificationLatencyMetric(long elapsedTimeSecs)
-	{
-		if( _log.isDebugEnabled() )
-		{
-			_log.debug("Elapsed time to add: " + elapsedTimeSecs);
-		}
-		
-		_latencyMetric._totalElapsedTime.addAndGet(elapsedTimeSecs);
-		_latencyMetric._numDataPoints.incrementAndGet();
-	}
-	
-	public static float computeAverageNotificationLatencySecs()
-	{
-		return _latencyMetric.computeAverageLatency();
-	}
-	
 	public static long getNanosPerSimulatedTimeIncrement()
 	{
 		return _nanosPerTimeIncrement;
-	}
-
-	
-	////////////////////////////// INNER CLASSES ///////////////////////////////
-	private static class Latency
-	{
-		AtomicLong _totalElapsedTime = new AtomicLong(0);
-		AtomicInteger _numDataPoints = new AtomicInteger(0);
-		
-		
-		public float computeAverageLatency()
-		{
-			float ret = _totalElapsedTime.get() / (float)_numDataPoints.get();
-			return ret; 				
-		}
-		
-		public void resetMetrics()
-		{
-			_totalElapsedTime.set(0);
-			_numDataPoints.set(0);
-		}
 	}
 }
