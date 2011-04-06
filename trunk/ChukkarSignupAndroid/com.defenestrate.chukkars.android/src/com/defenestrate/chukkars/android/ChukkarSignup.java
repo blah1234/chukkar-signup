7572366ab1aca800365091fb45fb760ab8bd0040
hwang.shawn@gmail.com <hwang.shawn@gmail.com@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-04-06 22:24:49 +0000

    add functionality to query last webapp reset date

--- trunk/ChukkarSignupAndroid/com.defenestrate.chukkars.android/src/com/defenestrate/chukkars/android/ChukkarSignup.java
+++ trunk/ChukkarSignupAndroid/com.defenestrate.chukkars.android/src/com/defenestrate/chukkars/android/ChukkarSignup.java
@@ -1,6 +1,26 @@
 package com.defenestrate.chukkars.android;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.defenestrate.chukkars.android.persistence.SignupDbAdapter;
+
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -9,6 +29,7 @@ import android.widget.TabHost;
 public class ChukkarSignup extends TabActivity
 {
 	//////////////////////////////// CONSTANTS /////////////////////////////////
+	static private final String RESET_DATE_FILENAME = "last-reset-date.txt";
 	static final String TAB_INDEX_KEY = "TAB_INDEX_KEY"; 
 
 
@@ -17,6 +38,9 @@ public class ChukkarSignup extends TabActivity
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		
+		queryWebAppResetDate();
+		
 	    setContentView(R.layout.signup_tabs);
 
 	    Resources res = getResources(); // Resource object to get Drawables
@@ -42,5 +66,167 @@ public class ChukkarSignup extends TabActivity
 	    tabHost.addTab(spec);
 
 	    tabHost.setCurrentTab(0);
+	}
+	
+	/**
+	 * See when the last time all signup data in the webapp was reset, and clear
+	 * out any saved player ids in local DB accordingly 
+	 */
+	private void queryWebAppResetDate()
+	{
+		Thread thread = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				InputStream is = null;
+				
+				try
+				{
+					//http get
+					HttpClient httpclient = new DefaultHttpClient();
+					HttpGet get = new HttpGet( getResources().getString(R.string.query_reset_url) );
+					HttpResponse response = httpclient.execute(get);
+					
+					HttpEntity entity = response.getEntity();
+					is = entity.getContent();
+			
+					//convert response to string
+			    	BufferedReader reader = new BufferedReader( new InputStreamReader(is,"utf-8") );
+			    	StringWriter strWrite = new StringWriter();
+			    	
+			    	String line = null;
+			    	while( (line = reader.readLine()) != null ) 
+			    	{
+			    		strWrite.append(line + "\n");
+			    	}
+				            
+			    	String result = strWrite.toString().trim();
+			    	
+			    	//get rid of quotes
+			    	if( result.startsWith("\"") )
+			    	{
+			    		result = result.substring(1);
+			    	}
+			    	
+			    	if( result.endsWith("\"") )
+			    	{
+			    		result = result.substring(0, result.length() - 1);
+			    	}
+			    	
+			    	DateFormat inParser = DateFormat.getDateTimeInstance();
+			    	Date resetDate = inParser.parse(result);
+			    	Date prevResetDate = getPreviousWebAppResetDate();
+			    	
+			    	if( (prevResetDate == null) || resetDate.after(prevResetDate) )
+			    	{
+			    		writeResetDate(result);
+			    		SignupDbAdapter db = new SignupDbAdapter(ChukkarSignup.this);
+			    		db.open();
+			    		db.deleteAllPlayers();
+			    		db.close();
+			    	}
+			    }
+				catch(IOException e)
+				{
+					//TODO:
+		            throw new RuntimeException(e);
+				}
+				catch(ParseException e)
+				{
+					//TODO:
+		            throw new RuntimeException(e);
+				}
+			}
+		});
+		
+        thread.start();
+    }
+	
+	private Date getPreviousWebAppResetDate()
+	{
+		String[] existingFiles = fileList();
+		boolean doesFileExist = false;
+    	
+    	for(String currFilename : existingFiles)
+    	{
+    		if( RESET_DATE_FILENAME.equals(currFilename) )
+    		{
+    			doesFileExist = true;
+    			break;
+    		}
+    	}
+    	
+    	if(doesFileExist)
+    	{
+			FileInputStream fis = null;
+			
+			try
+			{
+				fis = openFileInput(RESET_DATE_FILENAME);
+				BufferedReader reader = new BufferedReader( new InputStreamReader(fis, "utf-8") );
+				StringWriter strWrite = new StringWriter();
+		    	
+		    	String line = null;
+		    	while( (line = reader.readLine()) != null ) 
+		    	{
+		    		strWrite.append(line + "\n");
+		    	}
+			            
+		    	String result = strWrite.toString();
+		    	
+		    	DateFormat inParser = DateFormat.getDateTimeInstance();
+		    	Date prevResetDate = inParser.parse(result);
+		    	
+		    	return prevResetDate;
+	    	}
+	    	catch(Exception e)
+	    	{
+	    		//TODO:
+	            throw new RuntimeException(e);
+	    	}
+	    	finally
+	    	{
+	    		try
+		    	{
+		    		if(fis != null)
+		    		{
+		    			fis.close();
+		    		}
+		    	}
+		    	catch(IOException e) {}
+	    	}
+    	}
+
+    	
+    	return null;
+	}
+	
+	private void writeResetDate(String resetDate)
+	{
+		FileOutputStream fos = null;
+		
+		try
+		{
+	    	//write json data to file
+	    	fos = openFileOutput(RESET_DATE_FILENAME, Context.MODE_PRIVATE);
+	    	fos.write( resetDate.getBytes() );
+	    	fos.flush();
+		}
+		catch(IOException e)
+		{
+			// TODO:
+			throw new RuntimeException(e);
+		}
+	    finally
+	    {
+	    	try
+	    	{
+	    		if(fos != null)
+	    		{
+	    			fos.close();
+	    		}
+	    	}
+	    	catch(IOException e) {}
+	    }
 	}
 }
