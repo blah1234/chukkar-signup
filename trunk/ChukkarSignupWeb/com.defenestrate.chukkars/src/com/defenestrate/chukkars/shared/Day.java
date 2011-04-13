94fde36e142f2498205b086024cad04a077a6afb
hwang.shawn@gmail.com <hwang.shawn@gmail.com@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-04-13 23:13:52 +0000

    support arbitrary days

--- trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/shared/Day.java
+++ trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/shared/Day.java
@@ -1,7 +1,58 @@
 package com.defenestrate.chukkars.shared;
 
+
 public enum Day
 {
-	SATURDAY,
-	SUNDAY
+	//////////////////////////////// CONSTANTS /////////////////////////////////
+	MONDAY(1),
+	TUESDAY(2),
+	WEDNESDAY(3),
+	THURSDAY(4),
+	FRIDAY(5),
+	SATURDAY(6),
+	SUNDAY(7);
+	
+	
+	//////////////////////////// MEMBER VARIABLES //////////////////////////////
+	private int _numRepresentation;
+	private boolean _isEnabled;
+	
+	
+	/////////////////////////////// CONSTRUCTORS ///////////////////////////////
+	Day(int numRepresentation)
+	{
+		_numRepresentation = numRepresentation;
+		_isEnabled = false;
+	}
+	
+	
+	///////////////////////////////// METHODS //////////////////////////////////
+	static public Day[] getAll()
+	{
+		return new Day[] 
+		{ 
+			MONDAY, 
+			TUESDAY, 
+			WEDNESDAY, 
+			THURSDAY, 
+			FRIDAY, 
+			SATURDAY, 
+			SUNDAY 
+		};
+	}
+	
+	public boolean isEnabled()
+	{
+		return _isEnabled;
+	}
+	
+	public void setEnabled(boolean isEnabled)
+	{
+		_isEnabled = isEnabled;
+	}
+	
+	public int getNumber()
+	{
+		return _numRepresentation;
+	}
 }
