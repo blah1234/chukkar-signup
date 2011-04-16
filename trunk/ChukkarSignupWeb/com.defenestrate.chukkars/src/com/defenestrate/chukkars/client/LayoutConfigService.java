739281b094212155bdd874bbe562fd3727b63821
hwang.shawn@gmail.com <hwang.shawn@gmail.com@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-04-16 04:58:55 +0000

    support configurable active days

---
+++ trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/LayoutConfigService.java
@@ -0,0 +1,18 @@
+package com.defenestrate.chukkars.client;
+
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("layoutConfig")
+public interface LayoutConfigService extends RemoteService 
+{
+	/**
+	 * Returns a list strings representing all days marked active in the DB. 
+	 * No guarantee is made as to the order of the day strings within the 
+	 * returned list.
+	 * @return a list of all days marked active in the DB.
+	 */
+	List<String> getActiveDays();
+}
