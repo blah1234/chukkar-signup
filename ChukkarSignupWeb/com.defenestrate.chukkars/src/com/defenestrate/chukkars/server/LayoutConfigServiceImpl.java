38accfd935467503c5e0efec13f64dc13e772df2
hwang.shawn@gmail.com <hwang.shawn@gmail.com@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-04-18 22:23:11 +0000

    support closing signups

--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/LayoutConfigServiceImpl.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/LayoutConfigServiceImpl.java
@@ -1,18 +1,26 @@
 package com.defenestrate.chukkars.server;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
 import com.defenestrate.chukkars.client.LayoutConfigService;
+import com.defenestrate.chukkars.server.entity.CronTask;
 import com.defenestrate.chukkars.server.entity.DayOfWeek;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class LayoutConfigServiceImpl extends RemoteServiceServlet 
 							  		 implements LayoutConfigService 
 {
+	////////////////////////////////CONSTANTS /////////////////////////////////
+	private static final Logger LOG = Logger.getLogger( LayoutConfigServiceImpl.class.getName() );
+	
+	
 	///////////////////////////////// METHODS //////////////////////////////////
 	public List<String> getActiveDays() 
 	{
@@ -37,5 +45,40 @@ public class LayoutConfigServiceImpl extends RemoteServiceServlet
 		}
 		
 		return retList;
+	}
+	
+	public Date getSignupClosed()
+	{
+		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
+		
+		//default is "never"
+		Date ret = new Date(Long.MAX_VALUE);
+		
+		try
+		{
+		    Query q = pm.newQuery(CronTask.class);
+			q.setFilter("_name == name");
+			q.declareParameters("String name");
+			q.setUnique(true);
+			CronTask currTask = (CronTask)q.execute(CronTask.CLOSE_SIGNUP);
+
+			if(currTask != null) 
+			{
+				ret = currTask.getRunDate();
+			}
+		}
+		catch(Exception e)
+		{
+			LOG.log(
+				Level.SEVERE, 
+				"Error encountered trying to find task " + CronTask.CLOSE_SIGNUP + ":\n" + e.getMessage(), 
+				e);
+		}
+		finally 
+		{
+			pm.close();
+		}
+		
+		return ret;
 	}
 }
