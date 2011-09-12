dc85d9749a0e81d7bcaaec129fe857cb6d619484
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-09-12 06:33:51 +0000

    added doAdminsExist() method

--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/AdminService.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/AdminService.java
@@ -7,11 +7,12 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("admin")
-public interface AdminService extends RemoteService 
+public interface AdminService extends RemoteService
 {
 	MessageAdminClientCopy getMessageData(LoginInfo currLogin);
 	void saveMessageData(MessageAdminClientCopy data);
 	String exportSignups();
 	String importLineup(Day dayOfWeek);
 	void createAdminUser(String emailAddress, String nickname);
+	Boolean doAdminsExist();
 }
--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/AdminServiceAsync.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/AdminServiceAsync.java
@@ -5,11 +5,12 @@ import com.defenestrate.chukkars.shared.LoginInfo;
 import com.defenestrate.chukkars.shared.MessageAdminClientCopy;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-public interface AdminServiceAsync 
+public interface AdminServiceAsync
 {
 	void getMessageData(LoginInfo currLogin, AsyncCallback<MessageAdminClientCopy> async);
 	void saveMessageData(MessageAdminClientCopy data, AsyncCallback<Void> async);
 	void exportSignups(AsyncCallback<String> async);
 	void importLineup(Day dayOfWeek, AsyncCallback<String> async);
 	void createAdminUser(String emailAddress, String nickname, AsyncCallback<Void> async);
+	void doAdminsExist(AsyncCallback<Boolean> async);
 }
