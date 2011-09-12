cc7ed815151d83576e7b0d008389bd3006d4a579
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-09-12 06:50:43 +0000

    better doc

--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/shared/Day.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/shared/Day.java
@@ -3,6 +3,10 @@ package com.defenestrate.chukkars.shared;
 
 public enum Day
 {
+	/**
+	 * Use {@link #Day(int, boolean)} constructor below to set active days
+	 * when testing with local Java App Engine datastore.
+	 */
 	//////////////////////////////// CONSTANTS /////////////////////////////////
 	MONDAY(1),
 	TUESDAY(2),
@@ -11,53 +15,53 @@ public enum Day
 	FRIDAY(5),
 	SATURDAY(6),
 	SUNDAY(7);
-	
-	
+
+
 	//////////////////////////// MEMBER VARIABLES //////////////////////////////
 	private int _numRepresentation;
 	private boolean _isEnabled;
-	
-	
+
+
 	/////////////////////////////// CONSTRUCTORS ///////////////////////////////
 	private Day(int numRepresentation)
 	{
 		_numRepresentation = numRepresentation;
 		_isEnabled = false;
 	}
-	
+
 	private Day(int numRepresentation, boolean isEnabled)
 	{
 		_numRepresentation = numRepresentation;
 		_isEnabled = isEnabled;
 	}
-	
-	
+
+
 	///////////////////////////////// METHODS //////////////////////////////////
 	static final public Day[] getAll()
 	{
-		return new Day[] 
-		{ 
+		return new Day[]
+		{
 			//must be returned in lexicographically ascending order!
-			MONDAY, 
-			TUESDAY, 
-			WEDNESDAY, 
-			THURSDAY, 
-			FRIDAY, 
-			SATURDAY, 
-			SUNDAY 
+			MONDAY,
+			TUESDAY,
+			WEDNESDAY,
+			THURSDAY,
+			FRIDAY,
+			SATURDAY,
+			SUNDAY
 		};
 	}
-	
+
 	public boolean isEnabled()
 	{
 		return _isEnabled;
 	}
-	
+
 	public void setEnabled(boolean isEnabled)
 	{
 		_isEnabled = isEnabled;
 	}
-	
+
 	public int getNumber()
 	{
 		return _numRepresentation;
