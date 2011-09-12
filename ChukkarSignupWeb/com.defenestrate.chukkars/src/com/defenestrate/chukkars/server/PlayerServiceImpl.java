63d13d98e31e7fec78f0bb6b6c805f12901a7965
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-09-12 06:50:21 +0000

    better error handling

--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/PlayerServiceImpl.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/PlayerServiceImpl.java
@@ -17,13 +17,13 @@ import com.defenestrate.chukkars.shared.Day;
 import com.defenestrate.chukkars.shared.Player;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class PlayerServiceImpl extends RemoteServiceServlet 
-							   implements PlayerService 
+public class PlayerServiceImpl extends RemoteServiceServlet
+							   implements PlayerService
 {
 	//////////////////////////////// CONSTANTS /////////////////////////////////
 	private static final Logger LOG = Logger.getLogger( PlayerServiceImpl.class.getName() );
-	
-	
+
+
 	///////////////////////////////// METHODS //////////////////////////////////
 	/**
 	 * Adds a player
@@ -31,44 +31,60 @@ public class PlayerServiceImpl extends RemoteServiceServlet
 	 * @param numChukkars number of chukkars the player is requesting
 	 * @return persisted Player object
 	 */
-	public Player addPlayer(String name, Integer numChukkars, Day requestDay)  
+	@Override
+	public Player addPlayer(String name, Integer numChukkars, Day requestDay)
 	{
 		return addPlayerImpl(name, numChukkars, requestDay);
 	}
-	
+
 	static protected Player addPlayerImpl(String name, Integer numChukkars, Day requestDay)
 	{
 		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-		
-		try 
+
+		Player currPlayer = null;
+		Transaction tx = pm.currentTransaction();
+
+		try
 		{
-			Player currPlayer = new Player(name, numChukkars, requestDay);
+			tx.begin();
+
+			currPlayer = new Player(name, numChukkars, requestDay);
 			pm.makePersistent(currPlayer);
-			
-			return currPlayer;
+
+			tx.commit();
 		}
-/**@todo		catch(Exception e)
+		catch(Exception e)
 		{
 			LOG.log(
-				Level.SEVERE, 
-				"Error encountered trying to add a player (" + name + "; " + numChukkars + "; " + requestDay + ")", 
+				Level.SEVERE,
+				"Error encountered trying to add a player (" + name + "; " + numChukkars + "; " + requestDay + ")",
 				e);
-		}*/
-		finally 
+
+			if( tx.isActive() )
+		    {
+		        tx.rollback();
+		    }
+
+		    throw new RuntimeException(e);
+		}
+		finally
 		{
 			pm.close();
 		}
+
+		return currPlayer;
 	}
-	
-	public void editChukkars(Long playerId, Integer numChukkars) 
+
+	@Override
+	public void editChukkars(Long playerId, Integer numChukkars)
 	{
 		editChukkarsImpl(playerId, numChukkars);
 	}
-	
+
 	static protected Player editChukkarsImpl(Long playerId, Integer numChukkars)
 	{
 		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-		
+
 		Player editPlayer = null;
 		Transaction tx = pm.currentTransaction();
 
@@ -78,123 +94,135 @@ public class PlayerServiceImpl extends RemoteServiceServlet
 
 		    editPlayer = pm.getObjectById(Player.class, playerId);
 
-			if(editPlayer != null) 
+			if(editPlayer != null)
 			{
 				editPlayer.setChukkarCount(numChukkars);
 			}
 			else
 			{
-				LOG.log( Level.SEVERE, 
-						 "Player not found with id = " + playerId,
-						 new Throwable().fillInStackTrace() );
-			}		    
+				throw new IllegalArgumentException("Player not found with id = " + playerId);
+			}
 
 		    tx.commit();
 		}
 		catch(Exception e)
 		{
 			LOG.log(
-				Level.SEVERE, 
-				"Error encountered trying to modify number of chukkars for player with id = " + playerId + ":\n" + e.getMessage(), 
+				Level.SEVERE,
+				"Error encountered trying to modify number of chukkars for player with id = " + playerId + ":\n" + e.getMessage(),
 				e);
-			
+
 		    if( tx.isActive() )
 		    {
 		        tx.rollback();
 		    }
+
+		    throw new RuntimeException(e);
 		}
-		finally 
+		finally
 		{
 			pm.close();
 		}
-		
+
 		return editPlayer;
 	}
 
-	public void removePlayer(Long id) 
+	@Override
+	public void removePlayer(Long id)
 	{
 		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-		
+
 		Transaction tx = pm.currentTransaction();
-    
-		try 
+
+		try
 		{
 			tx.begin();
-			
+
 			Player delPlayer = pm.getObjectById(Player.class, id);
 
-			if(delPlayer != null) 
+			if(delPlayer != null)
 			{
 				pm.deletePersistent(delPlayer);
 			}
 			else
 			{
-				LOG.log( Level.SEVERE, 
-						 "Player not found with id = " + id,
-						 new Throwable().fillInStackTrace() );
+				throw new IllegalArgumentException("Player not found with id = " + id);
 			}
-			
+
 			tx.commit();
-		} 
+		}
 		catch(Exception e)
 		{
 			LOG.log(
-				Level.SEVERE, 
-				"Error encountered trying to remove player with id = " + id + ":\n" + e.getMessage(), 
+				Level.SEVERE,
+				"Error encountered trying to remove player with id = " + id + ":\n" + e.getMessage(),
 				e);
-			
+
 		    if( tx.isActive() )
 		    {
 		        tx.rollback();
 		    }
+
+		    throw new RuntimeException(e);
 		}
-		finally 
+		finally
 		{
 			pm.close();
 		}
 	}
-	
+
+	@Override
 	public List<Player> getPlayers()
 	{
 		return getPlayersImpl();
 	}
-	
-	static protected List<Player> getPlayersImpl() 
+
+	static protected List<Player> getPlayersImpl()
 	{
 		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-		
+
 		List<Player> retList = new ArrayList<Player>();
-		
-		try 
+
+		try
 		{
 			Extent<Player> e = pm.getExtent(Player.class);
 			Iterator<Player> iter = e.iterator();
-			
+
 			//sort in ascending chronological order
 			Comparator<Player> dateComp = new Comparator<Player>()
 			{
+				@Override
 				public int compare(Player o1, Player o2)
 				{
 					return o1.getCreateDate().compareTo( o2.getCreateDate() );
 				}
 			};
-			
+
 			while( iter.hasNext() )
 			{
 				Player currPlayer = iter.next();
-				
+
 				int index = Collections.binarySearch(retList, currPlayer, dateComp);
-				
+
 				//index = (-(insertion point) - 1);
 				int insertIndex = (index + 1) * -1;
 				retList.add(insertIndex, currPlayer);
 			}
-		} 
-		finally 
+		}
+		catch(Exception e)
+		{
+			LOG.log(
+				Level.SEVERE,
+				"Error encountered trying to get all players:\n" + e.getMessage(),
+				e);
+
+		    throw new RuntimeException(e);
+		}
+		finally
 		{
 			pm.close();
 		}
-		
+
 		return retList;
 	}
 }
