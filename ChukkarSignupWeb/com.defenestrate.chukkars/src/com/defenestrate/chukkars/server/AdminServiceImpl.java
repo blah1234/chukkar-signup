46407bf34f083612e57d65ed43d46a068ede1df1
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-09-12 06:46:19 +0000

    -better error handling -support resource bundles -implement doAdminsExist() method

--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/AdminServiceImpl.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/AdminServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,6 +23,11 @@ import com.defenestrate.chukkars.shared.Day;
 import com.defenestrate.chukkars.shared.LoginInfo;
 import com.defenestrate.chukkars.shared.MessageAdminClientCopy;
 import com.defenestrate.chukkars.shared.Player;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.gdata.client.spreadsheet.FeedURLFactory;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.data.PlainTextConstruct;
@@ -42,25 +48,26 @@ import com.google.gdata.util.AuthenticationException;
 import com.google.gdata.util.ServiceException;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class AdminServiceImpl extends RemoteServiceServlet 
-							  implements AdminService 
+public class AdminServiceImpl extends RemoteServiceServlet
+							  implements AdminService
 {
 	//////////////////////////////// CONSTANTS /////////////////////////////////
 	private static final Logger LOG = Logger.getLogger( AdminServiceImpl.class.getName() );
-	
+
 
 	//////////////////////////// MEMBER VARIABLES //////////////////////////////
 	private SpreadsheetService _service;
 
 
 	///////////////////////////////// METHODS //////////////////////////////////
-	public MessageAdminClientCopy getMessageData(LoginInfo currLogin) 
+	@Override
+	public MessageAdminClientCopy getMessageData(LoginInfo currLogin)
 	{
 		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-		
+
 		MessageAdminClientCopy ret = null;
-		
-		try 
+
+		try
 		{
 			Query q = pm.newQuery(MessageAdmin.class);
 			q.setFilter("_loginInfo == currLogin");
@@ -68,28 +75,38 @@ public class AdminServiceImpl extends RemoteServiceServlet
 			q.declareParameters("LoginInfo currLogin");
 			q.setUnique(true);
 			MessageAdmin data = (MessageAdmin)q.execute(currLogin);
-			
-			if(data != null) 
+
+			if(data != null)
 			{
-				ret = new MessageAdminClientCopy( data.getId(), 
-												  data.isWeeklyEmailsEnabled(), 
-												  data.getRecipientEmailAddress(), 
-												  data.getSignupNoticeMessage(), 
+				ret = new MessageAdminClientCopy( data.getId(),
+												  data.isWeeklyEmailsEnabled(),
+												  data.getRecipientEmailAddress(),
+												  data.getSignupNoticeMessage(),
 												  data.getSignupReminderMessage() );
 			}
 		}
-		finally 
+		catch(Exception e)
+		{
+			LOG.log(
+				Level.SEVERE,
+				"Error encountered trying to get email settings:\n" + e.getMessage(),
+				e);
+
+		    throw new RuntimeException(e);
+		}
+		finally
 		{
 			pm.close();
 		}
-		
+
 		return ret;
 	}
-	
+
+	@Override
 	public void saveMessageData(MessageAdminClientCopy data)
 	{
 		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-			
+
 		Transaction tx = pm.currentTransaction();
 
 		try
@@ -98,7 +115,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 
 		    MessageAdmin persistData = pm.getObjectById( MessageAdmin.class, data.getId() );
 
-			if(persistData != null) 
+			if(persistData != null)
 			{
 				persistData.setRecipientEmailAddress( data.getRecipientEmailAddress() );
 				persistData.setSignupNoticeMessage( data.getSignupNoticeMessage() );
@@ -107,9 +124,9 @@ public class AdminServiceImpl extends RemoteServiceServlet
 			}
 			else
 			{
-				LOG.log( Level.SEVERE, 
-						 "MessageAdmin not found with id = " + data.getId(), 
-						 new Throwable().fillInStackTrace() );
+				String msg = "MessageAdmin not found with id = " + data.getId();
+				IllegalArgumentException e = new IllegalArgumentException(msg);
+				throw e;
 			}
 
 		    tx.commit();
@@ -117,21 +134,23 @@ public class AdminServiceImpl extends RemoteServiceServlet
 		catch(Exception e)
 		{
 			LOG.log(
-				Level.SEVERE, 
-				"Error encountered trying to save the state of MessageAdmin with id = " + data.getId() + ":\n" + e.getMessage(), 
+				Level.SEVERE,
+				"Error encountered trying to save the state of MessageAdmin with id = " + data.getId() + ":\n" + e.getMessage(),
 				e);
-			
+
 		    if( tx.isActive() )
 		    {
 		        tx.rollback();
 		    }
+
+		    throw new RuntimeException(e);
 		}
-		finally 
+		finally
 		{
 			pm.close();
 		}
 	}
-	
+
 	private SpreadsheetService getDefaultSpreadsheetService() throws AuthenticationException
 	{
 		if(_service == null)
@@ -139,10 +158,10 @@ public class AdminServiceImpl extends RemoteServiceServlet
 			_service = new SpreadsheetService("com.defenesetrate.chukkars");
 			_service.setUserCredentials("horseparkpolo@gmail.com", "polo7656");
 		}
-		
+
 		return _service;
 	}
-	
+
 	private SpreadsheetEntry getLineupsEntry() throws MalformedURLException,
 													  AuthenticationException,
 													  ServiceException,
@@ -153,7 +172,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 		SpreadsheetFeed feed = service.getFeed(metafeedUrl, SpreadsheetFeed.class);
 		List<SpreadsheetEntry> spreadsheets = feed.getEntries();
 		SpreadsheetEntry lineupsEntry = null;
-		for(int i = 0; i < spreadsheets.size(); i++) 
+		for(int i = 0; i < spreadsheets.size(); i++)
 		{
 			SpreadsheetEntry entry = spreadsheets.get(i);
 			if("Chukkar lineups".equals(entry.getTitle().getPlainText()) )
@@ -162,14 +181,15 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				break;
 			}
 		}
-		
+
 		return lineupsEntry;
 	}
-	
+
+	@Override
 	public String exportSignups()
 	{
 		List<Player> allPlayersList = null;
-		
+
 		try
 		{
 			allPlayersList = PlayerServiceImpl.getPlayersImpl();
@@ -179,20 +199,20 @@ public class AdminServiceImpl extends RemoteServiceServlet
 			LOG.log(Level.SEVERE,
 					"Error getting all signed up players",
 					e);
-			
+
 			throw new RuntimeException(e);
 		}
-		
-		
+
+
 		try
 		{
 			SpreadsheetService service = getDefaultSpreadsheetService();
 			SpreadsheetEntry lineupsEntry = getLineupsEntry();
-			
+
 			//------------------
-			
-			List<Day> worksheetTitles = getActiveDays(); 
-			
+
+			List<Day> worksheetTitles = getActiveDays();
+
 			for(Day currTitle : worksheetTitles)
 			{
 				//calculate # of game chukkars
@@ -205,14 +225,14 @@ public class AdminServiceImpl extends RemoteServiceServlet
 					{
 						totalChukkars += currPlayer.getChukkarCount();
 						numPlayersPerDay++;
-						
+
 						if( maxPlayerNumChukkars < currPlayer.getChukkarCount() )
 						{
 							maxPlayerNumChukkars = currPlayer.getChukkarCount();
 						}
 					}
 				}
-				
+
 				int numGameChukkars;
 				if(numPlayersPerDay < 4)
 				{
@@ -226,14 +246,14 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				{
 					numGameChukkars = totalChukkars / 4;
 				}
-				
-				
+
+
 				if(numGameChukkars < maxPlayerNumChukkars)
 				{
 					numGameChukkars = maxPlayerNumChukkars;
 				}
-				
-				
+
+
 				try
 				{
 					//delete old lineup
@@ -247,14 +267,14 @@ public class AdminServiceImpl extends RemoteServiceServlet
 							break;
 						}
 					}
-					
+
 					//create a new blank worksheet for the new lineup
 					WorksheetEntry newWorksheet = new WorksheetEntry();
 					newWorksheet.setTitle( new PlainTextConstruct(currTitle.toString()) );
 					newWorksheet.setRowCount(numPlayersPerDay + 10);
 					newWorksheet.setColCount(numGameChukkars + 3);
 					service.insert(worksheetFeedUrl, newWorksheet);
-					
+
 					//populate with players and chukkars
 					TableEntry tableEntry = new TableEntry();
 
@@ -268,7 +288,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 
 					// Specify columns in the table, start row, number of rows.
 					Data tableData = new Data();
-					//for now tableData represents the table header columns, so 
+					//for now tableData represents the table header columns, so
 					//there are technically "0" rows of data afterwards. This is
 					//done so that when we start adding record entries, the data
 					//rows will be added directly under the table header columns
@@ -278,19 +298,19 @@ public class AdminServiceImpl extends RemoteServiceServlet
 
 					// populate the table header columns
 					tableData.addColumn( new Column("A", "Name") );
-					
+
 					char currCol='B';
 					int numChukkarCols = numGameChukkars + 1;
 					for(int i=1; i<=numChukkarCols; i++)
 					{
 						tableData.addColumn( new Column(Character.toString(currCol++), Integer.toString(i)) );
 					}
-					
+
 					tableEntry.setData(tableData);
 					TableEntry insertedTable = service.insert(tableFeedUrl, tableEntry);
-					
+
 					//--------------------
-					
+
 					//populate with players and chukkars
 					String[] parts = insertedTable.getId().split("\\/");
 					String tableId = parts[parts.length - 1];
@@ -300,9 +320,9 @@ public class AdminServiceImpl extends RemoteServiceServlet
 					colorEntry.addField( new Field(null, "Name", "light") );
 					padRemainingColumns(colorEntry, 1, numChukkarCols);
 					service.insert(recordFeedUrl, colorEntry);
-					
-					for(int i=0, n=allPlayersList.size(), halfWay=numPlayersPerDay/2, currPlayerCount=0; 
-						i<n; 
+
+					for(int i=0, n=allPlayersList.size(), halfWay=numPlayersPerDay/2, currPlayerCount=0;
+						i<n;
 						i++)
 					{
 						if(currPlayerCount == halfWay)
@@ -320,32 +340,32 @@ public class AdminServiceImpl extends RemoteServiceServlet
 							colorEntry.addField( new Field(null, "Name", "dark") );
 							padRemainingColumns(colorEntry, 1, numChukkarCols);
 							service.insert(recordFeedUrl, colorEntry);
-							
+
 							//get counter variable off of "halfway" mark
 							currPlayerCount++;
 						}
-						
-						
+
+
 						Player currPlayer = allPlayersList.get(i);
-												
+
 						if(currPlayer.getRequestDay() == currTitle)
 						{
 							if(currPlayer.getChukkarCount() > 0)
 							{
 								RecordEntry playerEntry = new RecordEntry();
 								playerEntry.addField( new Field(null, "Name", currPlayer.getName()) );
-								
+
 								int j=1;
 								for(int m=currPlayer.getChukkarCount(); j<=m; j++)
 								{
 									playerEntry.addField( new Field(null, Integer.toString(j), "x") );
 								}
-								
+
 								padRemainingColumns(playerEntry, j, numChukkarCols);
-								
+
 								service.insert(recordFeedUrl, playerEntry);
 							}
-							
+
 							currPlayerCount++;
 						}
 					}
@@ -356,7 +376,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 						Level.SEVERE,
 						"Error manipulating Google spreadsheet",
 						e);
-					
+
 					throw new RuntimeException(e);
 				}
 				catch(IOException e)
@@ -365,11 +385,11 @@ public class AdminServiceImpl extends RemoteServiceServlet
 						Level.SEVERE,
 						"Error manipulating Google spreadsheet",
 						e);
-					
+
 					throw new RuntimeException(e);
 				}
 			}
-			
+
 			return lineupsEntry.getSpreadsheetLink().getHref();
 		}
 		catch(MalformedURLException e)
@@ -378,7 +398,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"https://spreadsheets.google.com/feeds/spreadsheets/private/full considered malformed",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 		catch(AuthenticationException e)
@@ -387,7 +407,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"Unable to authenticate for Google spreadsheets",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 		catch(ServiceException e)
@@ -396,7 +416,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"Error opening \"Chukkar lineups\" Google spreadsheet",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 		catch(IOException e)
@@ -405,16 +425,16 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"Error opening \"Chukkar lineups\" Google spreadsheet",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	private List<Day> getActiveDays()
 	{
 		Day[] allDays = Day.getAll();
 		List<Day> retList = new ArrayList<Day>();
-		
+
 		for(Day currDay : allDays)
 		{
 			if( currDay.isEnabled() )
@@ -422,10 +442,10 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				retList.add(currDay);
 			}
 		}
-		
+
 		return retList;
 	}
-	
+
 	private void padRemainingColumns(RecordEntry entry, int colStart, int colEnd)
 	{
 		for(int i=colStart; i<=colEnd; i++)
@@ -433,7 +453,8 @@ public class AdminServiceImpl extends RemoteServiceServlet
 			entry.addField( new Field(null, Integer.toString(i), " ") );
 		}
 	}
-	
+
+	@Override
 	public String importLineup(Day dayOfWeek)
 	{
 		try
@@ -444,8 +465,8 @@ public class AdminServiceImpl extends RemoteServiceServlet
 			URL tableFeedUrl = factory.getTableFeedUrl(lineupsEntry.getKey());
 			TableFeed feed = service.getFeed(tableFeedUrl, TableFeed.class);
 			TableEntry lineupTblEntry = null;
-			
-			for( TableEntry entry : feed.getEntries() ) 
+
+			for( TableEntry entry : feed.getEntries() )
 			{
 				if( entry.getTitle().getPlainText().equalsIgnoreCase(dayOfWeek.toString() + " Lineup Table") )
 				{
@@ -453,24 +474,25 @@ public class AdminServiceImpl extends RemoteServiceServlet
 					break;
 				}
 			}
-			
+
 			String[] parts = lineupTblEntry.getId().split("\\/");
 			String tableId = parts[parts.length - 1];
 			URL recordFeedUrl = factory.getRecordFeedUrl(lineupsEntry.getKey(), tableId);
 			RecordFeed recordFeed = service.getFeed(recordFeedUrl, RecordFeed.class);
-	
+
 			StringBuffer strBuf = new StringBuffer();
-			
-			for( RecordEntry entry : recordFeed.getEntries() ) 
+
+			for( RecordEntry entry : recordFeed.getEntries() )
 			{
 				List<Field> fieldList = entry.getFields();
 				Collections.sort(fieldList, new Comparator<Field>()
 				{
+					@Override
 					public int compare(Field f1, Field f2)
 					{
 						boolean isF1Digit = Character.isDigit( f1.getName().charAt(0) );
 						boolean isF2Digit = Character.isDigit( f2.getName().charAt(0) );
-						
+
 						if(!isF1Digit && isF2Digit)
 						{
 							return -1;
@@ -481,18 +503,19 @@ public class AdminServiceImpl extends RemoteServiceServlet
 						}
 						else
 						{
-							return new Integer(Integer.parseInt(f1.getName())).compareTo( 
+							return new Integer(Integer.parseInt(f1.getName())).compareTo(
 								new Integer(Integer.parseInt(f2.getName())) );
 						}
 					}
-					
+
+					@Override
 					public boolean equals(Object o)
 					{
 						return super.equals(o);
 					}
 				});
-				
-				for(Field currField : fieldList) 
+
+				for(Field currField : fieldList)
 				{
 					if( currField.getName().equalsIgnoreCase("Name") )
 					{
@@ -505,16 +528,16 @@ public class AdminServiceImpl extends RemoteServiceServlet
 						strBuf.append(",");
 					}
 				}
-				
+
 				if(strBuf.charAt(strBuf.length()-1) == ',')
 				{
 					strBuf.setLength(strBuf.length() - 1);
 				}
-				
+
 				strBuf.append("\n");
 			}
-			
-			
+
+
 			return strBuf.toString();
 		}
 		catch(MalformedURLException e)
@@ -523,7 +546,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"https://spreadsheets.google.com/feeds/spreadsheets/private/full considered malformed",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 		catch(AuthenticationException e)
@@ -532,7 +555,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"Unable to authenticate for Google spreadsheets",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 		catch(ServiceException e)
@@ -541,7 +564,7 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"Error opening \"Chukkar lineups\" Google spreadsheet",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 		catch(IOException e)
@@ -550,15 +573,16 @@ public class AdminServiceImpl extends RemoteServiceServlet
 				Level.SEVERE,
 				"Error opening \"Chukkar lineups\" Google spreadsheet",
 				e);
-			
+
 			throw new RuntimeException(e);
 		}
 	}
-	
+
+	@Override
 	public void createAdminUser(String emailAddress, String nickname)
 	{
 		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-		
+
 		Transaction tx = pm.currentTransaction();
 
 		try
@@ -569,33 +593,47 @@ public class AdminServiceImpl extends RemoteServiceServlet
 			loginInfo.setIsAdmin(true);
 			loginInfo.setEmailAddress(emailAddress);
 			loginInfo.setNickname(nickname);
-			
+
 		    MessageAdmin persistData = new MessageAdmin();
 		    persistData.setAdmin(loginInfo);
-		    persistData.setRecipientEmailAddress("horseparkpolo@yahoogroups.com");
+		    ResourceBundle strings = ResourceBundle.getBundle("com.defenestrate.chukkars.shared.resources.DisplayStrings");
+		    persistData.setRecipientEmailAddress( strings.getString("clubListEmail") );
 			persistData.setWeeklyEmailsEnabled(false);
 			persistData.setSignupNoticeMessage("");
 			persistData.setSignupReminderMessage("");
-			
+
 			pm.makePersistent(persistData);
-			
+
 		    tx.commit();
 		}
 		catch(Exception e)
 		{
 			LOG.log(
-				Level.SEVERE, 
-				"Error encountered trying to create a new admin: (" + emailAddress + ", " + nickname + ")", 
+				Level.SEVERE,
+				"Error encountered trying to create a new admin: (" + emailAddress + ", " + nickname + ")",
 				e);
-			
+
 		    if( tx.isActive() )
 		    {
 		        tx.rollback();
 		    }
+
+		    throw new RuntimeException(e);
 		}
-		finally 
+		finally
 		{
 			pm.close();
 		}
+	}
+
+	@Override
+	public Boolean doAdminsExist()
+	{
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+	    com.google.appengine.api.datastore.Query q = new com.google.appengine.api.datastore.Query("MessageAdmin").setKeysOnly();
+	    PreparedQuery pq = ds.prepare(q);
+	    List<Entity> results = pq.asList(FetchOptions.Builder.withLimit(1));
+
+		return !results.isEmpty();
 	}
 }
