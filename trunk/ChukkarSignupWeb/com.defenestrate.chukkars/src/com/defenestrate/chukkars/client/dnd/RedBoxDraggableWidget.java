509ebcf17b10e66ae1fdf69e629afc949d2ae046
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-10-04 06:40:44 +0000

    move from client package to client.dnd package

--- trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/dnd/RedBoxDraggableWidget.java
+++ trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/dnd/RedBoxDraggableWidget.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2009 Fred Sauer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.defenestrate.chukkars.client;
+package com.defenestrate.chukkars.client.dnd;
 
 import com.google.gwt.user.client.ui.HTML;
 
@@ -24,11 +24,11 @@ public final class RedBoxDraggableWidget extends HTML {
 
   private static final String CSS_DEMO_RED_BOX_DRAGGABLE_WIDGET = "demo-red-box-draggable-widget";
 
-  private static final int DRAGGABLE_SIZE = 65;
+  public static final int DRAGGABLE_SIZE = 75;
 
-  public RedBoxDraggableWidget() {
+  public RedBoxDraggableWidget(String label) {
     setPixelSize(DRAGGABLE_SIZE, DRAGGABLE_SIZE);
-    setHTML("<i>drag me!</i> draggable widget #" + ++counter);
+    setHTML(label);
   }
 
   @Override
