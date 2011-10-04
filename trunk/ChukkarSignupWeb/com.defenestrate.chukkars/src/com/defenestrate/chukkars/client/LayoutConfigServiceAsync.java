000176913a5de17db3c62af5a575a5caceb69408
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-10-04 06:40:11 +0000

    add saveDaysConfig() method

--- trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/LayoutConfigServiceAsync.java
+++ trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/LayoutConfigServiceAsync.java
@@ -2,11 +2,13 @@ package com.defenestrate.chukkars.client;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-public interface LayoutConfigServiceAsync 
+public interface LayoutConfigServiceAsync
 {
 	void getActiveDays(AsyncCallback<List<String>> async);
+	void saveDaysConfig(Set<String> activeDayNames, AsyncCallback<Void> async);
 	void getSignupClosed(AsyncCallback<Date> async );
 }
