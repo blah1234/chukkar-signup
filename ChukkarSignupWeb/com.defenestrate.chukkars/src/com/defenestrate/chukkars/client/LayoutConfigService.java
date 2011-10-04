a5df80f97585f4d2e4f9e90ff679085e3d14c071
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-10-04 06:39:46 +0000

    add saveDaysConfig() method

--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/LayoutConfigService.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/LayoutConfigService.java
@@ -2,23 +2,32 @@ package com.defenestrate.chukkars.client;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
+import com.defenestrate.chukkars.shared.Day;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("layoutConfig")
-public interface LayoutConfigService extends RemoteService 
+public interface LayoutConfigService extends RemoteService
 {
 	/**
-	 * Returns a list strings representing all days marked active in the DB. 
-	 * No guarantee is made as to the order of the day strings within the 
+	 * Returns a list strings representing all days marked active in the DB.
+	 * No guarantee is made as to the order of the day strings within the
 	 * returned list.
 	 * @return a list of all days marked active in the DB.
 	 */
 	List<String> getActiveDays();
-	
+
 	/**
-	 * Returns the date and time past which signup is closed  
+	 * Saves the state of the {@link Day} enumeration to <code>DayOfWeek</code>
+	 * entities in the DB.
+	 * @param activeDayNames set of names of all active days
+	 */
+	void saveDaysConfig(Set<String> activeDayNames);
+
+	/**
+	 * Returns the date and time past which signup is closed
 	 * @return the date and time past which signup is closed
 	 */
 	Date getSignupClosed();
