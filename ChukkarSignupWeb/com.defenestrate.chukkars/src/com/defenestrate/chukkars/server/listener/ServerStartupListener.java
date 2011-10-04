07242e201de706b64728e6290188506d96d57403
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2011-10-04 03:32:44 +0000

    don't need reinit and close persistence stuff in each iteration of the loop.

--- ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/listener/ServerStartupListener.java
+++ ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/listener/ServerStartupListener.java
@@ -19,31 +19,32 @@ public class ServerStartupListener implements ServletContextListener
 
 	///////////////////////////////// METHODS //////////////////////////////////
 	/**
-	 * This method is invoked when the Web Application is ready to service requests 
+	 * This method is invoked when the Web Application is ready to service requests
 	 */
+	@Override
 	public void contextInitialized(ServletContextEvent event)
 	{
 		loadDaysConfiguration();
 	}
-	
+
 	static public void loadDaysConfiguration()
 	{
 		//initialize persisted state of Day enums
 		Day[] allDays = Day.getAll();
-		
-		for(Day currDay : allDays)
+
+		//find the Day in the DB and copy over persisted state
+		PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
+
+		try
 		{
-			//find the Day in the DB and copy over persisted state
-			PersistenceManager pm = PersistenceManagerHelper.getPersistenceManager();
-		    
-			try 
+			for(Day currDay : allDays)
 			{
 				Query q = pm.newQuery(DayOfWeek.class);
 				q.setFilter("_numRepresentation == number");
 				q.declareParameters("int number");
 				q.setUnique(true);
 				DayOfWeek persistedDay = (DayOfWeek)q.execute( currDay.getNumber() );
-				
+
 				if(persistedDay != null)
 				{
 					currDay.setEnabled( persistedDay.isEnabled() );
@@ -52,22 +53,23 @@ public class ServerStartupListener implements ServletContextListener
 				{
 					//go ahead and create the Day in the DB
 					LOG.info("Did not find : " + currDay + " in DB. Creating it now.");
-					
+
 					DayOfWeek newPersistDay = new DayOfWeek(
-						currDay.toString(), 
-						currDay.getNumber(), 
+						currDay.toString(),
+						currDay.getNumber(),
 						currDay.isEnabled() );
-					
+
 					pm.makePersistent(newPersistDay);
 				}
-			} 
-			finally 
-			{
-				pm.close();
 			}
-		}		
+		}
+		finally
+		{
+			pm.close();
+		}
 	}
-	
+
+	@Override
 	public void contextDestroyed(ServletContextEvent event)
 	{
 	}
