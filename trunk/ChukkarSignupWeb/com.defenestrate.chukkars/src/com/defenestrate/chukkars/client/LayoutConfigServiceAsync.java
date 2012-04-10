6ffb75b0bf21dc3b04ebc4ccaedd7f59af0af6a7
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2012-04-10 19:03:42 +0000

    support multiple cutoff times for different days

--- trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/LayoutConfigServiceAsync.java
+++ trunk/ChukkarSignupWeb/com.defenestrate.chukkars/src/com/defenestrate/chukkars/client/LayoutConfigServiceAsync.java
@@ -2,13 +2,15 @@ package com.defenestrate.chukkars.client;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import com.defenestrate.chukkars.shared.Day;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface LayoutConfigServiceAsync
 {
 	void getActiveDays(AsyncCallback<List<String>> async);
 	void saveDaysConfig(Set<String> activeDayNames, AsyncCallback<Void> async);
-	void getSignupClosed(AsyncCallback<Date> async );
+	void getSignupClosed(AsyncCallback<Map<Day, Date>> async);
 }
