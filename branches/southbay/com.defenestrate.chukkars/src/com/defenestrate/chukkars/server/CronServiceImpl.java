98d27d1d2a4880ae123ea3f312a57d1a98549f05
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2012-08-10 20:25:59 +0000

    finalize settings for southbay

--- branches/southbay/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/CronServiceImpl.java
+++ branches/southbay/com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/CronServiceImpl.java
@@ -170,7 +170,7 @@ public class CronServiceImpl extends HttpServlet
         					cal.add(Calendar.DAY_OF_WEEK, 1);
         				} while(cal.get(Calendar.DAY_OF_WEEK) != lastDay);
 
-        				cal.set(Calendar.HOUR_OF_DAY, 12);	 //noon
+        				cal.set(Calendar.HOUR_OF_DAY, 19);	 //7pm
         				cal.set(Calendar.MINUTE, 30);
 
         				logTask( CronTask.CLOSE_SIGNUP + currDay.getNumber(), cal.getTime() );
@@ -475,7 +475,7 @@ public class CronServiceImpl extends HttpServlet
 		{
 			String msgBody = data.getSignupReminderMessage();
 			ResourceBundle strings = ResourceBundle.getBundle("com.defenestrate.chukkars.shared.resources.DisplayStrings");
-			EmailServiceImpl.sendEmail(strings.getString("clubAbbreviation") + " signup by 12 noon", msgBody, data, false);
+			EmailServiceImpl.sendEmail(strings.getString("clubAbbreviation") + " signup by 7pm", msgBody, data, false);
 		}
 
 		//------------------
