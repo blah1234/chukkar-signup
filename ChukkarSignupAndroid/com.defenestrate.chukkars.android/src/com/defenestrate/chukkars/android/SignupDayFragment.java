21638cb59164650077b66784ed9900e22a3c80aa
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2013-05-29 06:14:32 +0000

    flesh out impl

--- ChukkarSignupAndroid/com.defenestrate.chukkars.android/src/com/defenestrate/chukkars/android/SignupDayFragment.java
+++ ChukkarSignupAndroid/com.defenestrate.chukkars.android/src/com/defenestrate/chukkars/android/SignupDayFragment.java
@@ -1,10 +1,6 @@
 package com.defenestrate.chukkars.android;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.MessageFormat;
@@ -19,7 +15,6 @@ import java.util.Set;
 import java.util.TimeZone;
 import java.util.TreeSet;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -28,16 +23,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -50,6 +49,7 @@ import android.widget.Toast;
 import com.defenestrate.chukkars.android.entity.Day;
 import com.defenestrate.chukkars.android.exception.SignupClosedException;
 import com.defenestrate.chukkars.android.util.Constants;
+import com.defenestrate.chukkars.android.util.HttpUtil;
 import com.defenestrate.chukkars.android.util.PlayerSignupData;
 import com.defenestrate.chukkars.android.util.PropertiesUtil;
 import com.defenestrate.chukkars.android.widget.FancyScrollListAdapter.FancyScrollListSubadapter;
@@ -63,9 +63,6 @@ public class SignupDayFragment extends FancyScrollListFragment
 
 	/////////////////////////////// CONSTANTS //////////////////////////////////
 	private static final String LOG_TAG = "SignupDayFragment";
-	static final String PAGE_INDEX_KEY = "PAGE_INDEX_KEY";
-	static final String SIGNUP_DAY_KEY = "SIGNUP_DAY_KEY";
-	static private final String SIGNUP_CLOSED = "!!!SIGNUP_CLOSED!!!";
 	static private final int MAX_NUM_COVER_ART = 12;
 
 
@@ -76,9 +73,16 @@ public class SignupDayFragment extends FancyScrollListFragment
     private Day _selectedDay;
     private List<PlayerSignupData> mPlayerSignupList;
     private int mTotalGameChukkars;
+    private AsyncTask<Integer, Void, Integer> mTask;
+    private boolean mViewDestroyed = false;
+    private OnPlayerModificationListener mCallback;
+    private Handler _errHandler;
+    private ActionMode.Callback mActionModeCallback;
+    private ActionMode mActionMode;
+    private View mPrevSelectedView;
+    private OnPageChangeListener mOnPageChangeLstnr;
 
     static private final Random mRand = new Random();
-    static private Handler _errHandler;
     static private final Set<Integer> sUsedCoverArtIds = new TreeSet<Integer>();
 
 
@@ -88,18 +92,46 @@ public class SignupDayFragment extends FancyScrollListFragment
         return R.layout.fancy_scroll_signup_list;
     }
 
+	@Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnPlayerModificationListener)activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnActivityLaunchListener");
+        }
+
+        if(activity instanceof ViewPagerActivity) {
+    		mOnPageChangeLstnr = new ViewPager.SimpleOnPageChangeListener() {
+
+    			@Override
+    			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    				closeActionMode();
+            	}
+    		};
+
+            ( (ViewPagerActivity)activity ).addOnPageChangeListener(mOnPageChangeLstnr);
+        } else {
+            mOnPageChangeLstnr = null;
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mPlayerSignupList = new ArrayList<PlayerSignupData>();
 
-        if(_errHandler == null) {
-        	_errHandler = new Handler( Looper.getMainLooper() )
+    	_errHandler = new Handler( Looper.getMainLooper() )
+        {
+            @Override
+            public void handleMessage(Message msg)
             {
-                @Override
-                public void handleMessage(Message msg)
-                {
+            	if(!mViewDestroyed) {
                     if( (msg.what == R.id.message_what_error) && (msg.arg1 != 0) )
                     {
                 		ErrorToast.show( getActivity(),
@@ -113,19 +145,57 @@ public class SignupDayFragment extends FancyScrollListFragment
                     	Toast toast = Toast.makeText(getActivity(), text, duration);
                     	toast.show();
                     }
+            	}
+            }
+        };
+
+        mActionModeCallback = new ActionMode.Callback() {
+
+            // Called when the action mode is created; startActionMode() was called
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                // Inflate a menu resource providing context menu items
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.context_menu, menu);
+                return true;
+            }
+
+            // Called each time the action mode is shown. Always called after onCreateActionMode, but
+            // may be called multiple times if the mode is invalidated.
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false; // Return false if nothing is done
+            }
+
+            // Called when the user selects a contextual menu item
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+//                    case R.id.menu_share:
+//                        shareCurrentItem();
+//                        mode.finish(); // Action picked, so close the CAB
+//                        return true;
+                    default:
+                        return false;
                 }
-            };
-        }
+            }
+
+            // Called when the user exits the action mode
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+            	mActionMode = null;
+                closeActionMode();
+            }
+        };
     }
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     	View ret = super.onCreateView(inflater, container, savedInstanceState);
 
-        Resources res = getResources();
     	SharedPreferences settings = getActivity().getSharedPreferences(SERVER_DATA_PREFS_NAME, Context.MODE_PRIVATE);
-        String data = settings.getString(res.getString(R.string.content_key), null);
-        long lastModified = settings.getLong(res.getString(R.string.last_modified_key), 0);
+        String data = settings.getString(CONTENT_KEY, null);
+        long lastModified = settings.getLong(LAST_MODIFIED_KEY, 0);
 
     	boolean doesDataExist = (data != null);
 
@@ -141,8 +211,26 @@ public class SignupDayFragment extends FancyScrollListFragment
 		return ret;
     }
 
+    public void onStart() {
+    	super.onStart();
+    	int i=0;
+    	i++;
+    }
+
     @Override public void onDestroyView() {
     	super.onDestroyView();
+
+    	mViewDestroyed = true;
+
+    	if(mTask != null) {
+    		mTask.cancel(true);
+    		mTask = null;
+    	}
+
+    	if( (mOnPageChangeLstnr != null) &&
+    		(getActivity() instanceof ViewPagerActivity) ) {
+    		( (ViewPagerActivity)getActivity() ).removeOnPageChangeListener(mOnPageChangeLstnr);
+    	}
 
     	sUsedCoverArtIds.remove(mCoverArtId);
     }
@@ -163,6 +251,10 @@ public class SignupDayFragment extends FancyScrollListFragment
 
     private void getServerData(int tabIndex)
 	{
+    	if(mTask != null) {
+    		mTask.cancel(true);
+    	}
+
 		AsyncTask<Integer, Void, Integer> task = new AsyncTask<Integer, Void, Integer>()
 		{
 			@Override
@@ -174,8 +266,6 @@ public class SignupDayFragment extends FancyScrollListFragment
 		    @Override
 		    protected Integer doInBackground(Integer... params)
 		    {
-		    	Resources res = getResources();
-
 				try
 				{
 					//http get
@@ -183,7 +273,9 @@ public class SignupDayFragment extends FancyScrollListFragment
 					HttpGet get = new HttpGet( PropertiesUtil.getURLProperty(getResources(), "get_players_url") );
 					HttpResponse response = httpclient.execute(get);
 
-					writeServerData(response);
+					if( !isCancelled() ) {
+						_dataLastModified = HttpUtil.writeServerData(response, getActivity());
+					}
 
 					Integer tabIndexArg = params[0];
 					return tabIndexArg;
@@ -214,6 +306,7 @@ public class SignupDayFragment extends FancyScrollListFragment
 		    @Override
 		    protected void onPostExecute(Integer result)
 		    {
+		    	mTask = null;
 		    	showLoading(false);
 
 		    	if(result != null)
@@ -223,65 +316,14 @@ public class SignupDayFragment extends FancyScrollListFragment
 		    }
 		};
 
+		mTask = task;
 		task.execute(tabIndex);
-	}
-
-    private void writeServerData(HttpResponse response) throws SignupClosedException, IOException
-	{
-		InputStream is = null;
-
-		try
-		{
-			HttpEntity entity = response.getEntity();
-			is = entity.getContent();
-
-			//convert response to string
-	    	BufferedReader reader = new BufferedReader( new InputStreamReader(is,"utf-8") );
-	    	StringWriter strWrite = new StringWriter();
-
-	    	String line = null;
-	    	while( (line = reader.readLine()) != null )
-	    	{
-	    		strWrite.append(line + "\n");
-	    	}
-
-	    	String result = strWrite.toString();
-
-	    	if( SIGNUP_CLOSED.equals(result.trim()) )
-	    	{
-	    		throw new SignupClosedException();
-	    	}
-
-	    	//write json data to preferences
-	    	Resources res = getResources();
-			SharedPreferences settings = getActivity().getSharedPreferences(SERVER_DATA_PREFS_NAME, Context.MODE_PRIVATE);
-		    SharedPreferences.Editor editor = settings.edit();
-		    editor.putString(res.getString(R.string.content_key), result);
-
-		    _dataLastModified = new Date().getTime();
-		    editor.putLong(res.getString(R.string.last_modified_key), _dataLastModified);
-
-		    // Commit the edits!
-		    editor.commit();
-		}
-	    finally
-	    {
-	    	try
-	    	{
-	    		if(is != null)
-	    		{
-	    			is.close();
-	    		}
-	    	}
-	    	catch(IOException e) {}
-	    }
 	}
 
     private void loadPlayersImpl(int pageIndex)
     {
-    	Resources res = getResources();
     	SharedPreferences settings = getActivity().getSharedPreferences(SERVER_DATA_PREFS_NAME, Context.MODE_PRIVATE);
-        String data = settings.getString(res.getString(R.string.content_key), null);
+        String data = settings.getString(CONTENT_KEY, null);
 
         if(data != null)
         {
@@ -297,9 +339,8 @@ public class SignupDayFragment extends FancyScrollListFragment
     	//parse json data
 	    try
 	    {
-	    	Resources res = getResources();
 	    	JSONObject data = new JSONObject(result);
-	    	JSONArray jArray = data.getJSONArray( res.getString(R.string.totals_list_field) );
+	    	JSONArray jArray = data.getJSONArray(TOTALS_LIST_FIELD);
 
 	    	if(jArray.length() <= pageIndex)
 	    	{
@@ -315,9 +356,9 @@ public class SignupDayFragment extends FancyScrollListFragment
 				return;
 	    	}
 
-	    	String dayStr = jArray.getJSONObject(pageIndex).getString( res.getString(R.string.total_day_field) );
+	    	String dayStr = jArray.getJSONObject(pageIndex).getString(TOTAL_DAY_FIELD);
 	    	_selectedDay = Day.valueOf(dayStr);
-	    	mTotalGameChukkars = jArray.getJSONObject(pageIndex).getInt( res.getString(R.string.total_num_chukkars_field) );
+	    	mTotalGameChukkars = jArray.getJSONObject(pageIndex).getInt(TOTAL_NUM_CHUKKARS_FIELD);
 
 	    	if( (mListAdapter != null) && (mListAdapter instanceof FancyScrollSignupDayListAdapter) ) {
 	    		Bundle args = new Bundle();
@@ -339,20 +380,20 @@ public class SignupDayFragment extends FancyScrollListFragment
             outFormatter.setTimeZone( TimeZone.getTimeZone("America/Los_Angeles") );
 
             mPlayerSignupList.clear();
-            jArray = data.getJSONArray( res.getString(R.string.players_list_field) );
+            jArray = data.getJSONArray(PLAYERS_LIST_FIELD);
 
             for(int i=0, n=jArray.length(); i<n; i++)
             {
                 JSONObject currPlayer = jArray.getJSONObject(i);
 
-                Day currRequestDay = Day.valueOf( currPlayer.getString( res.getString(R.string.player_requestDay_field)) );
+                Day currRequestDay = Day.valueOf( currPlayer.getString( PLAYER_REQUESTDAY_FIELD) );
                 if(currRequestDay == _selectedDay)
                 {
                 	String displayDateTime;
 
                 	try
             		{
-                		String createDate = currPlayer.getString( res.getString(R.string.player_createDate_field) );
+                		String createDate = currPlayer.getString( PLAYER_CREATEDATE_FIELD );
                 		Date dateTime = inParser.parse(createDate);
                 		displayDateTime = outFormatter.format(dateTime);
             		}
@@ -367,10 +408,10 @@ public class SignupDayFragment extends FancyScrollListFragment
             		}
 
                 	PlayerSignupData rowData = new PlayerSignupData(
-                		currPlayer.getString(res.getString(R.string.player_id_field)),
+                		currPlayer.getString(PLAYER_ID_FIELD),
                 		displayDateTime,
-                		currPlayer.getString(res.getString(R.string.player_name_field)),
-                		currPlayer.getString(res.getString(R.string.player_numChukkars_field)) );
+                		currPlayer.getString(PLAYER_NAME_FIELD),
+                		currPlayer.getString(PLAYER_NUMCHUKKARS_FIELD) );
 
                 	mPlayerSignupList.add(rowData);
                 }
@@ -421,7 +462,12 @@ public class SignupDayFragment extends FancyScrollListFragment
     }
 
     private Drawable getAssignedCoverArt() {
-		String fieldName = "cover" + mCoverArtId;
+    	int id = getAssignedCoverArtId();
+    	return getResources().getDrawable(id);
+    }
+
+    private int getAssignedCoverArtId() {
+    	String fieldName = "cover" + mCoverArtId;
 		int id;
 
 		try {
@@ -441,7 +487,7 @@ public class SignupDayFragment extends FancyScrollListFragment
 			id = R.drawable.cover1;
 		}
 
-    	return getResources().getDrawable(id);
+		return id;
     }
 
     /**
@@ -550,7 +596,23 @@ public class SignupDayFragment extends FancyScrollListFragment
         args.putString( FancyScrollSignupDayListAdapter.LOWER_LABEL_2_STRING, getString(R.string.game_chukkars_loading) );
 
 
-        FancyScrollListSubadapter subadapter = new SignupDayListSubadapter(mPlayerSignupList, getActivity(), this);
+        FancyScrollListSubadapter subadapter = new SignupDayListSubadapter(mPlayerSignupList, getActivity(), this) {
+
+        	@Override
+        	protected void onHeaderClick(View v) {
+                launchAddPlayerPage();
+            }
+
+        	@Override
+        	protected void onItemClick(View v, int position, long id) {
+        		closeActionMode();
+
+                // Start the CAB using the ActionMode.Callback defined above
+                mActionMode = getActivity().startActionMode(mActionModeCallback);
+                v.setSelected(true);
+                mPrevSelectedView = v;
+        	}
+        };
 
         FancyScrollSignupDayListAdapter adapter = new FancyScrollSignupDayListAdapter(getActivity(), inflater, this, parent, args, subadapter) {
 
@@ -563,25 +625,49 @@ public class SignupDayFragment extends FancyScrollListFragment
 				}
 		    }
 
-		    @Override
-			protected void onHeaderButton1Click(View v) {
-			}
-
-		    @Override
-			protected void onHeaderButton2Click(View v) {
-		    }
-
-			@Override
-			protected void onHeaderButton3Click(View v) {
-			}
-
 			@Override
 			protected void onHeaderCircleButtonClick(View v) {
+				launchAddPlayerPage();
 			}
 
 		};
 
 		return adapter;
+	}
+
+	private void closeActionMode() {
+		if(mPrevSelectedView != null) {
+			mPrevSelectedView.setSelected(false);
+		}
+
+		if (mActionMode != null) {
+            mActionMode.finish();
+        }
+
+		mPrevSelectedView = null;
+		mActionMode = null;
+	}
+
+	private void launchAddPlayerPage() {
+		Intent i = new Intent(getActivity(), AddPlayerActivity.class);
+		i.putExtra(SIGNUP_DAY_KEY, _selectedDay);
+		i.putExtra( COVER_ART_KEY, getAssignedCoverArtId() );
+
+		startActivityForResult(i, R.id.get_server_data_request);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == R.id.get_server_data_request) {
+			if(resultCode == Activity.RESULT_CANCELED) {
+				mCallback.onPlayerModificationCancel();
+			} else if(resultCode == Activity.RESULT_OK) {
+				Day selectedDay = (Day)data.getSerializableExtra(SELECTED_DAY_KEY);
+				mCallback.onPlayerModificationSave(selectedDay);
+			} else {
+				throw new IllegalArgumentException("result code (" + resultCode + ") not recognized for request R.id.get_server_data_request");
+			}
+		}
 	}
 
 	@Override
@@ -592,5 +678,18 @@ public class SignupDayFragment extends FancyScrollListFragment
 		}
 
 	    return _selectedDay.name();
+	}
+
+
+	//////////////////////////// INNER CLASSES /////////////////////////////////
+	public interface OnPlayerModificationListener {
+		/**
+		 * Notifies the listener that a player modification has been saved
+		 * @param selectedDay Day that the modified player signed up for
+		 */
+		void onPlayerModificationSave(Day selectedDay);
+
+		/** Notifies the listener that a player modification has been canceled */
+		void onPlayerModificationCancel();
 	}
 }
