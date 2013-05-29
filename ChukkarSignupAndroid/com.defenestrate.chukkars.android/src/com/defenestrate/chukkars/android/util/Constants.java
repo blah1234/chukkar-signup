d35aabeeececb0cafecd7fe096f8a2f4efdfb8d0
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2013-05-29 06:15:37 +0000

    convenience utils

--- ChukkarSignupAndroid/com.defenestrate.chukkars.android/src/com/defenestrate/chukkars/android/util/Constants.java
+++ ChukkarSignupAndroid/com.defenestrate.chukkars.android/src/com/defenestrate/chukkars/android/util/Constants.java
@@ -2,4 +2,26 @@ package com.defenestrate.chukkars.android.util;
 
 public interface Constants {
 	String SERVER_DATA_PREFS_NAME = "all-players.json";
+	String SIGNUP_CLOSED = "!!!SIGNUP_CLOSED!!!";
+
+	String PAGE_INDEX_KEY = "PAGE_INDEX_KEY";
+	String SIGNUP_DAY_KEY = "SIGNUP_DAY_KEY";
+	String SELECTED_DAY_KEY = "SELECTED_DAY_KEY";
+	String COVER_ART_KEY = "COVER_ART_KEY";
+	String CONTENT_KEY = "CONTENT_KEY";
+    String LAST_MODIFIED_KEY = "LAST_MODIFIED_KEY";
+    String RESET_DATE_KEY = "RESET_DATE_KEY";
+    String ACTIVE_DAYS_KEY = "ACTIVE_DAYS_KEY";
+    String TOAST_MSG_KEY = "TOAST_MSG_KEY";
+
+    String TOTALS_LIST_FIELD = "_totalsList";
+    String PLAYERS_LIST_FIELD ="_playersList";
+    String CURR_PLAYER_PERSISTED_FIELD = "_currPersisted";
+    String TOTAL_DAY_FIELD = "_day";
+    String TOTAL_NUM_CHUKKARS_FIELD = "_numGameChukkars";
+    String PLAYER_ID_FIELD = "_id";
+    String PLAYER_NAME_FIELD = "_name";
+    String PLAYER_NUMCHUKKARS_FIELD = "_numChukkars";
+    String PLAYER_CREATEDATE_FIELD = "_createDate";
+    String PLAYER_REQUESTDAY_FIELD = "_requestDay";
 }
