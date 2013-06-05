ab37f8230a67f0940697520f8c86f1fee2cf3ec4
hwang.shawn <hwang.shawn@18fa5fbb-a5e2-92bc-02b7-a8c536cd345a>
2013-06-05 00:15:49 +0000

    -handle the corner case where Player.getExtent() doesn't always return a newly added Player -handle the corner case where android user tries to edit a player, but that player has already been deleted from the web app

--- com.defenestrate.chukkars/src/com/defenestrate/chukkars/shared/Player.java
+++ com.defenestrate.chukkars/src/com/defenestrate/chukkars/shared/Player.java
@@ -12,6 +12,9 @@ import javax.jdo.annotations.PrimaryKey;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.flipthebird.gwthashcodeequals.EqualsBuilder;
+import com.flipthebird.gwthashcodeequals.HashCodeBuilder;
+
 
 
 @PersistenceCapable(identityType = IdentityType.APPLICATION)
@@ -21,28 +24,28 @@ public class Player implements Serializable
 	@PrimaryKey
 	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
 	private Long _id;
-	
+
 	@Column(name = "Player_Name")
 	private String _name;
-	
+
 	@Column(name = "Player_Chukkars")
 	private Integer _numChukkars;
-	
+
 	@Column(name = "Player_CreateDate")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date _createDate;
-	
+
 	@Column(sqlType = "varchar(15)", name = "Player_RequestDay")
 	private Day _requestDay;
 
-	
+
 	/////////////////////////////// CONSTRUCTORS ///////////////////////////////
-	public Player() 
+	public Player()
 	{
 		_createDate = new Date();
 	}
 
-	public Player(String name, Integer numChukkars, Day requestDay) 
+	public Player(String name, Integer numChukkars, Day requestDay)
 	{
 		this();
 		_name = name;
@@ -50,43 +53,80 @@ public class Player implements Serializable
 		_requestDay = requestDay;
 	}
 
-	public Long getId() 
+	public Long getId()
 	{
 		return _id;
 	}
 
-	public String getName() 
+	public String getName()
 	{
 		return _name;
 	}
 
-	public Integer getChukkarCount() 
+	public Integer getChukkarCount()
 	{
 		return _numChukkars;
 	}
 
-	public Date getCreateDate() 	
+	public Date getCreateDate()
 	{
 		return _createDate;
 	}
-	
+
 	public Day getRequestDay()
 	{
 		return _requestDay;
 	}
 
-	public void setName(String name) 
+	public void setName(String name)
 	{
 		_name = name;
 	}
 
-	public void setChukkarCount(Integer numChukkars) 
+	public void setChukkarCount(Integer numChukkars)
 	{
 		_numChukkars = numChukkars;
 	}
-	
+
 	public void setRequestDay(Day requestDay)
 	{
 		_requestDay = requestDay;
+	}
+
+	public boolean equals(Object obj) {
+		if(obj == null) {
+			return false;
+		}
+
+		if(obj == this) {
+			return true;
+		}
+
+		if( obj.getClass() != getClass() ) {
+			return false;
+		}
+
+		Player rhs = (Player)obj;
+
+		return new EqualsBuilder()
+			.appendSuper(super.equals(obj))
+	        .append(_id, rhs._id)
+	        .append(_name, rhs._name)
+	        .append(_numChukkars, rhs._numChukkars)
+	        .append(_createDate, rhs._createDate)
+	        .append(_requestDay, rhs._requestDay)
+	        .isEquals();
+	}
+
+	public int hashCode() {
+		// pick a hard-coded, randomly chosen, non-zero, odd number
+	    // ideally different for each class
+		return new HashCodeBuilder(17, 37).
+			append(_id).
+			append(_name).
+			append(_numChukkars).
+			append(_createDate).
+			append(_requestDay).
+			toHashCode();
 	}
 }
