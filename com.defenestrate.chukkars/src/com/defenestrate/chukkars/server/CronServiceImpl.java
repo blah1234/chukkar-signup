02f35bda408f4755198623e178e2471bd032e243
shawn hwang <s.hwang1@samsung.com>
2018-05-25 12:26:48 -0700

    change chukkar sign up deadline back to noon for the summer

--- com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/CronServiceImpl.java
+++ com.defenestrate.chukkars/src/com/defenestrate/chukkars/server/CronServiceImpl.java
@@ -202,7 +202,7 @@ public class CronServiceImpl extends HttpServlet
         					cal.add(Calendar.DAY_OF_WEEK, 1);
         				} while(cal.get(Calendar.DAY_OF_WEEK) != lastDay);
 
-        				cal.set(Calendar.HOUR_OF_DAY, 17);	 //5pm
+        				cal.set(Calendar.HOUR_OF_DAY, 12);	 //12pm
         				cal.set(Calendar.MINUTE, 30);
 
         				logTask( CronTask.CLOSE_SIGNUP + currDay.getNumber(), cal.getTime() );
@@ -575,7 +575,7 @@ public class CronServiceImpl extends HttpServlet
 			{
 				String msgBody = data.getSignupReminderMessage();
 				ResourceBundle strings = ResourceBundle.getBundle("com.defenestrate.chukkars.shared.resources.DisplayStrings");
-				EmailServiceImpl.sendEmail(strings.getString("clubAbbreviation") + " signup by 5pm", msgBody, data, false);
+				EmailServiceImpl.sendEmail(strings.getString("clubAbbreviation") + " signup by 12 noon", msgBody, data, false);
 
 				//----------------
 
